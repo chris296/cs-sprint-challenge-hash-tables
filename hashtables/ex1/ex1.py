4a85e4ab7b7251cd9c58710d2e0659bbc5d24136
chris296 <chris.rios296@gmail.com>
2020-08-07 10:40:28 -0700

    first commit

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,17 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
-
-    return None
+    cache = {}
+    x = 0
+    y = 0
+    for idx, val in enumerate(weights):
+        cache[val] = idx
+    print("cache:", cache)
+    for idx, val in enumerate(weights):
+        if (limit - val) in cache and (limit - val) != idx:
+            x = max(cache[limit - val], idx)
+            y = min(cache[limit - val], idx)
+    if x == 0 and y == 0:
+        return None
+    print(x, y)
+    return (x, y)
